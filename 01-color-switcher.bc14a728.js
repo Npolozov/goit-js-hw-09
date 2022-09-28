337a3083f4ba30347926675cf7c851ee67465ad4
Npolozov <103377917+Npolozov@users.noreply.github.com>
2022-09-28 16:14:06 +0000

    Deploying to gh-pages from @ Npolozov/goit-js-hw-09@fc91dedfa8da9f0ebb1669f29e3b57837afb883b 🚀

---
+++ 01-color-switcher.bc14a728.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),o=document.querySelector("body"),n=null;console.log(e),console.log(t),e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,n=setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16));o.style.backgroundColor=e}),1e3)})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(n),console.log("Interval with id ".concat(n," has stopped!"))}))}();
+//# sourceMappingURL=01-color-switcher.bc14a728.js.map
