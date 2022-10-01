6945eb0c324d6b5933fd3ed794468ae911fd799d
Npolozov <103377917+Npolozov@users.noreply.github.com>
2022-10-01 19:24:52 +0000

    Deploying to gh-pages from @ Npolozov/goit-js-hw-09@dc338ca9837e29637a7311a43e505b83cbcc6d0f 🚀

--- 03-promises.9fcc544b.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=Math.random()>.3;console.log(o)}();
-//# sourceMappingURL=03-promises.9fcc544b.js.map
--- 03-promises.b3197a4a.js
+++
@@ -1,2 +0,0 @@
-const o=Math.random()>.3;console.log(o);
-//# sourceMappingURL=03-promises.b3197a4a.js.map
